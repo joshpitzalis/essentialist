d3fbad2815312f493c00ca968b958961baf030ad
Josh <jxsh@hey.com>
2026-01-23 09:24:17 +0530

    Acceptance-Testing-a-Walking-Skeleton

---
+++ 4_Acceptance-Testing-a-Walking-Skeleton/mine/packages/frontend/jest.config.e2e.ts
@@ -0,0 +1,22 @@
+import * as path from 'path';
+import type { JestConfigWithTsJest } from 'ts-jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from '../../tsconfig.json';
+
+export default async (): Promise<JestConfigWithTsJest> => ({
+  displayName: 'Frontend (E2E)',
+  testMatch: ['**/@(src|tests)/**/*.@(e2e).*'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['ts-jest', { 
+      diagnostics: false,
+      tsconfig: {
+        esModuleInterop: true
+      }
+    }],
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: path.resolve(__dirname, '../../'),
+  }),
+  passWithNoTests: true
+});
